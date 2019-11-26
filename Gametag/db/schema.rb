41308b3604f3ad48c34488a950340f97277fe986
Liverson Paulo <liverson.p@gmail.com>
2019-11-26 19:54:34 -0300

    Created a new model called home using scaffold and it changed the dependences

--- Gametag/db/schema.rb
+++ Gametag/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_200211) do
+ActiveRecord::Schema.define(version: 2019_11_26_225147) do
+
+  create_table "homes", force: :cascade do |t|
+    t.string "imagem"
+    t.text "descricao"
+    t.string "nome"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
