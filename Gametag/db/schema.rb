ac3aa02d96f312b6aba43282cbfc3af004083565
matheusafonsouza <matheusafonsouza@gmail.com>
2019-11-27 01:34:35 -0200

    created model Comment

--- Gametag/db/schema.rb
+++ Gametag/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_022110) do
+ActiveRecord::Schema.define(version: 2019_11_27_033412) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "post_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "homes", force: :cascade do |t|
     t.string "imagem"
@@ -25,6 +35,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_022110) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +50,6 @@ ActiveRecord::Schema.define(version: 2019_11_27_022110) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
