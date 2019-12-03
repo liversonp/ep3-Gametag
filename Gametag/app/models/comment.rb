e5d7dbc2811b15cea82d012de59ebe9b06e0bdca
matheusafonsouza <matheusafonsouza@gmail.com>
2019-12-03 03:57:46 -0200

    only commenter can delete comment

--- Gametag/app/models/comment.rb
+++ Gametag/app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
     belongs_to :home
+    belongs_to :user
 end
--- Gametag/db/schema.rb
+++ Gametag/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_233622) do
+ActiveRecord::Schema.define(version: 2019_12_03_055342) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_12_02_233622) do
     t.integer "home_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "homes", force: :cascade do |t|
